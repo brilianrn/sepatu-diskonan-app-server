d8bf20a5858b8497058d3a93d4827e209fec6b2c
brilianrn <brilian.rachmad13@gmail.com>
2021-03-28 15:37:02 +0700

    product controller done

--- controllers/productController.js
+++ controllers/productController.js
@@ -31,7 +31,14 @@ class ProductController{
   static getOneProduct(req, res, next){
     Product.findByPk(+req.params.productId)
     .then(data => {
-      res.status(200).json(data)
+      if (data == null) {
+        next({
+          name: 'notFound',
+          code: 403
+        })
+      } else {
+        res.status(200).json(data);
+      }
     })
     .catch(err => {
       next(err);
@@ -39,11 +46,44 @@ class ProductController{
   }
 
   static editProduct(req, res, next){
-    
+    let editProduct = {
+      name: req.body.name,
+      size: req.body.size,
+      color: req.body.color,
+      image_url: req.body.image_url
+    };
+
+    Product.update(editProduct, { where: { id: +req.params.productId }})
+    .then(data => {
+      if (data == 0) {
+        next({
+          name: 'notFound',
+          code: 403
+        })
+      } else {
+        res.status(200).json({ message: `Data with id: ${req.params.productId} successfully to updated`});
+      }
+    })
+    .catch(err => {
+      next(err)
+    })
   }
 
   static deleteProduct(req, res, next){
-    
+    Product.destroy({ where: { id: +req.params.productId }})
+    .then(data => {
+      if (data == 0) {
+        next({
+          name: 'notFound',
+          code: 403
+        })
+      } else {
+        res.status(200).json({ message: `Data with id: ${req.params.productId} successfully to deleted`});
+      }
+    })
+    .catch(err => {
+      next(err)
+    })
   }
 }
 
--- middlewares/errHandler.js
+++ middlewares/errHandler.js
@@ -1,12 +1,14 @@
 function errHandler(err, req, res, next) {
     if (err.name === 'wrongPass') {
-        res.status(err.code).json({ message: 'Invalid email/ password' });
+        res.status(400).json({ message: 'Invalid email/ password' });
     } else if (err.name === 'SequelizeValidationError') {
         res.status(400).json({ message: err.message })
     } else if (err.name === 'wrongAuth') {
-        res.status(err.code).json({ message: 'You are unauthorized!' })
+        res.status(401).json({ message: 'You are unauthorized!' })
     } else if (err.name === 'notLogin') {
-        res.status(err.code).json({ message: 'You must login first!'})
+        res.status(401).json({ message: 'You must login first!'})
+    } else if (err.name === 'notFound') {
+        res.status(404).json({ message: 'Data is not found!'})
     }
     else {
         res.status(500).json(err);
